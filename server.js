f74743824279aa55777f592c1c24ba1f0b3bf5f2
Nick Tsoukas <nick.tsoukas101@gmail.com>
2018-05-30 12:11:26 -0600

    Adding all files

---
+++ server.js
@@ -0,0 +1,60 @@
+// need home page
+// fav foods
+// movies
+// framworks
+
+// Dependencies
+var http = require("http");
+var fs = require("fs");
+
+// Set our port to 8080
+var PORT = 8080;
+
+// Create our server
+var server = http.createServer(handleRequest);
+
+// Create a function for handling the requests and responses coming into our server
+function handleRequest(req, res) {
+  var path = req.url;
+  switch (path) {
+  case "/":
+
+  fs.readFile(__dirname + "/index.html", function(err, data) {
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.end(data);
+  });
+
+  case "/foods":
+  fs.readFile(__dirname + "/foods.html", function(err, data) {
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.end(data);
+  });
+
+  case "/movies":
+  fs.readFile(__dirname + "/movies.html", function(err, data) {
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.end(data);
+  });
+
+  case "/frameworks":
+  fs.readFile(__dirname + "/frameworks.html", function(err, data) {
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.end(data);
+  });
+
+  }
+}
+
+
+// Starts our server
+server.listen(PORT, function() {
+  console.log("Server is listening on PORT: " + PORT);
+});
